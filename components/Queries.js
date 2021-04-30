ebcceb6f1a2dfe8300d423716ef464ed1bb96796
Sharad Chand <pepsighan@sharadchand.com>
2021-04-30 09:01:06 +0545

    Implement deleting queries

--- components/Queries.js
+++ components/Queries.js
@@ -1,11 +1,23 @@
-import { Box, Button, Text, useDisclosure } from '@chakra-ui/react';
+import { Box, Button, Flex, IconButton, Text, useDisclosure } from '@chakra-ui/react';
 import { useCallback } from 'react';
+import { MdDelete } from 'react-icons/md';
 import { useEditorState } from 'store/editor';
+import { useImmerSetter } from 'store/zustand';
 import QueryBuilderDialog from './graphqlQuery/QueryBuilderDialog';
 
 export default function Queries() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const savedQueries = useEditorState(useCallback((state) => state.savedQueries, []));
+
+  const updateEditorState = useImmerSetter(useEditorState);
+  const onQueryDelete = useCallback(
+    (id) => () => {
+      updateEditorState((state) => {
+        state.savedQueries = state.savedQueries.filter((it) => it.id !== id);
+      });
+    },
+    [updateEditorState]
+  );
 
   return (
     <>
@@ -15,7 +27,12 @@ export default function Queries() {
 
       <Box mb={4}>
         {savedQueries.map((it) => (
-          <Text key={it.id}>{it.variableName}</Text>
+          <Flex justifyContent="space-between" alignItems="center" mb={2}>
+            <Text key={it.id}>{it.variableName}</Text>
+            <IconButton size="xs" colorScheme="red" onClick={onQueryDelete(it.id)}>
+              <MdDelete />
+            </IconButton>
+          </Flex>
         ))}
 
         {savedQueries.length === 0 && <Text color="gray.500">There are no queries.</Text>}
