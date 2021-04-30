d9798858fcb573088b51bf6f61df5e92b0f7eb1e
Sharad Chand <pepsighan@sharadchand.com>
2021-04-30 09:24:31 +0545

    Move the related components to their own dir

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { Flex } from '@chakra-ui/layout';
 import { Editor } from '@craftjs/core';
 import components from 'canvasComponents';
-import Canvas from 'components/Canvas';
-import LeftSidebar from 'components/LeftSidebar';
-import Navigation from 'components/Navigation';
-import RightSidebar from 'components/RightSidebar';
+import Canvas from 'components/editor/Canvas';
+import LeftSidebar from 'components/editor/LeftSidebar';
+import Navigation from 'components/editor/Navigation';
+import RightSidebar from 'components/editor/RightSidebar';
 
 export default function Home() {
   return (
