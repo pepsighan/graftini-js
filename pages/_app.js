bc2749fb7e3893682bd37d4d3b63a1e555d880fd
Sharad Chand <pepsighan@sharadchand.com>
2021-05-04 17:08:04 +0545

    Listen to auth changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useMemo } from 'react';
+import { useAuthListener } from 'store/auth';
 import 'utils/firebase';
 import { initializeApollo } from 'utils/graphql';
 
@@ -8,6 +9,9 @@ export default function MyApp({ Component, pageProps }) {
   const apolloClient = useMemo(() => initializeApollo(pageProps.initialApolloState), [
     pageProps.initialApolloState,
   ]);
+
+  // Listens to the auth changes in the app. If a new user logs in or logs out.
+  useAuthListener();
 
   return (
     <ApolloProvider client={apolloClient}>
