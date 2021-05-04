39e27d209e8bdb0a87cf69f6cf6f89492266506e
Sharad Chand <pepsighan@sharadchand.com>
2021-05-04 16:52:07 +0545

    No func, call it directly

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useMemo } from 'react';
-import { initializeFirebase } from 'utils/firebase';
+import 'utils/firebase';
 import { initializeApollo } from 'utils/graphql';
-
-initializeFirebase();
 
 export default function MyApp({ Component, pageProps }) {
   const apolloClient = useMemo(() => initializeApollo(pageProps.initialApolloState), [
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-export function initializeFirebase() {
-  if (typeof window === 'undefined') {
-    return;
-  }
-
-  firebase.initializeApp({
-    apiKey: 'AIzaSyD-g9qamJTwYeDJRi80sP6X5qCCep07EMo',
-    authDomain: 'nocodepress-dev.firebaseapp.com',
-  });
-}
+firebase.initializeApp({
+  apiKey: 'AIzaSyD-g9qamJTwYeDJRi80sP6X5qCCep07EMo',
+  authDomain: 'nocodepress-dev.firebaseapp.com',
+});
