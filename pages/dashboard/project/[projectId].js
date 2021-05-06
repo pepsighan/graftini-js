73bf5c2424e70f4d1f9fbf73f34af987c8c22e45
Sharad Chand <pepsighan@sharadchand.com>
2021-05-06 20:52:27 +0545

    Load the project and if not found show not found

--- pages/dashboard/project/[projectId].js
+++ pages/dashboard/project/[projectId].js
@@ -6,37 +6,53 @@ import EditorNavigation from 'components/editor/EditorNavigation';
 import LeftSidebar from 'components/editor/LeftSidebar';
 import RightSidebar from 'components/editor/RightSidebar';
 import SEO from 'components/SEO';
-import { useCallback } from 'react';
+import { useRouter } from 'next/router';
+import NotFound from 'pages/404';
+import { useCallback, useMemo } from 'react';
 import { useEditorState } from 'store/editor';
+import { useMyProject } from 'store/projects';
 import { useImmerSetter } from 'store/zustand';
 import { protectedPage } from 'utils/auth';
+import { decodeSlug } from 'utils/url';
 
 export default protectedPage(function Project() {
+  const { query } = useRouter();
+  const projectId = useMemo(() => decodeSlug(query.projectId), [query.projectId]);
+
+  const { project, loading } = useMyProject({ projectId });
+
   const updateEditorState = useImmerSetter(useEditorState);
+  const onNodesChange = useCallback(
+    (query) => {
+      const serialized = query.getSerializedNodes();
+
+      // When the editor is first rendered, it returns an empty serialized object.
+      // We want to ignore it. The default nodes are going to be rendered which
+      // at least has a `ROOT` node.
+      const hasSome = Object.keys(serialized).length !== 0;
+
+      if (hasSome) {
+        updateEditorState((state) => {
+          state.markup = serialized;
+        });
+      }
+    },
+    [updateEditorState]
+  );
+
+  if (loading) {
+    // TODO: Show some skeleton loading of the editor here.
+    return null;
+  }
+
+  if (!project) {
+    return <NotFound />;
+  }
 
   return (
     <>
       <SEO title="Project" />
-      <Editor
-        resolver={components}
-        onNodesChange={useCallback(
-          (query) => {
-            const serialized = query.getSerializedNodes();
-
-            // When the editor is first rendered, it returns an empty serialized object.
-            // We want to ignore it. The default nodes are going to be rendered which
-            // at least has a `ROOT` node.
-            const hasSome = Object.keys(serialized).length !== 0;
-
-            if (hasSome) {
-              updateEditorState((state) => {
-                state.markup = serialized;
-              });
-            }
-          },
-          [updateEditorState]
-        )}
-      >
+      <Editor resolver={components} onNodesChange={onNodesChange}>
         <EditorNavigation />
         <Flex>
           <LeftSidebar />
--- utils/url.js
+++
@@ -1,5 +0,0 @@
-import { encode } from '@digitalmaas/uuid-base62';
-
-export function slugify({ id, name }) {
-  return `${name.replaceAll(/\s+/g, '-')}--${encode(id)}`;
-}
