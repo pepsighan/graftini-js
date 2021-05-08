c8807143d09f8b3813a2ec61f56c71b21de34e1b
Sharad Chand <pepsighan@sharadchand.com>
2021-05-08 17:34:34 +0545

    No need of saved queries here, gql is handling that

--- store/editor.tsx
+++ store/editor.tsx
@@ -3,7 +3,7 @@ import produce from 'immer';
 import { ReactNode, useState } from 'react';
 import create from 'zustand';
 import createContext from 'zustand/context';
-import { ProjectPage, Query } from './projects';
+import { ProjectPage } from './projects';
 import { WithImmerSetter } from './zustand';
 
 export enum RightSidebarOpenPane {
@@ -13,16 +13,7 @@ export enum RightSidebarOpenPane {
 
 type UseEditorState = {
   rightSidebarOpenPane: RightSidebarOpenPane;
-  savedQueries: SavedQuery[];
   pages: EditorPage[];
-};
-
-type SavedQuery = {
-  id: string;
-  variableName: string;
-  query: {
-    [field: string]: boolean;
-  };
 };
 
 type EditorPage = {
@@ -32,10 +23,9 @@ type EditorPage = {
 
 export const rootComponentId = 'ROOT';
 
-const createEditorState = (pages: ProjectPage[], queries: Query[]) =>
+const createEditorState = (pages: ProjectPage[]) =>
   create<WithImmerSetter<UseEditorState>>((set) => ({
     rightSidebarOpenPane: RightSidebarOpenPane.StyleOptions,
-    savedQueries: parseQueries(queries),
     pages: pages.map((it) => ({
       id: it.id,
       markup: parseMarkup(it.markup),
@@ -47,19 +37,14 @@ const { Provider, useStore } = createContext<WithImmerSetter<UseEditorState>>();
 
 type EditorStateProviderProps = {
   initialPages: ProjectPage[];
-  initialQueries: Query[];
   children: ReactNode;
 };
 
 /**
  * We need the editor state completely clean for each of the project.
  */
-export function EditorStateProvider({
-  initialPages,
-  initialQueries,
-  children,
-}: EditorStateProviderProps) {
-  const [initialStore] = useState(() => createEditorState(initialPages, initialQueries));
+export function EditorStateProvider({ initialPages, children }: EditorStateProviderProps) {
+  const [initialStore] = useState(() => createEditorState(initialPages));
   return <Provider initialStore={initialStore}>{children}</Provider>;
 }
 
@@ -67,8 +52,4 @@ export const useEditorState = useStore as ReturnType<typeof createEditorState>;
 
 export function parseMarkup(markup: string): SerializedNodes {
   return {};
-}
-
-function parseQueries(queries: Query[]): SavedQuery[] {
-  return [];
 }
