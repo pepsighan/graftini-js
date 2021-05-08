11884657e6f688b94c1655b71f55468084a3e019
Sharad Chand <pepsighan@sharadchand.com>
2021-05-08 17:21:31 +0545

    Break the component because the editor is dependent on the provider

---
+++ components/editor/Editor.js
@@ -0,0 +1,47 @@
+import { Flex } from '@chakra-ui/layout';
+import { Editor as Edt } from '@craftjs/core';
+import components from 'canvasComponents';
+import Canvas from 'components/editor/Canvas';
+import EditorNavigation from 'components/editor/EditorNavigation';
+import LeftSidebar from 'components/editor/LeftSidebar';
+import RightSidebar from 'components/editor/RightSidebar';
+import { useCallback, useMemo } from 'react';
+import { useEditorState } from 'store/editor';
+import { useImmerSetter } from 'store/zustand';
+import { initializeUserApollo, UserApolloProvider } from 'utils/graphqlUser';
+
+export default function Editor({ projectId }) {
+  const updateEditorState = useImmerSetter(useEditorState);
+  const onNodesChange = useCallback(
+    (query) => {
+      const serialized = query.getSerializedNodes();
+
+      // When the editor is first rendered, it returns an empty serialized object.
+      // We want to ignore it. The default nodes are going to be rendered which
+      // at least has a `ROOT` node.
+      const hasSome = Object.keys(serialized).length !== 0;
+
+      if (hasSome) {
+        updateEditorState((state) => {
+          state.markup = serialized;
+        });
+      }
+    },
+    [updateEditorState]
+  );
+
+  const userApollo = useMemo(() => initializeUserApollo(), []);
+
+  return (
+    <UserApolloProvider client={userApollo}>
+      <Edt resolver={components} onNodesChange={onNodesChange}>
+        <EditorNavigation />
+        <Flex>
+          <LeftSidebar projectId={projectId} />
+          <Canvas />
+          <RightSidebar />
+        </Flex>
+      </Edt>
+    </UserApolloProvider>
+  );
+}
