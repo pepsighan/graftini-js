d01ef85ff8ed25d72310523b8beec5bb19cdc6be
Sharad Chand <pepsighan@sharadchand.com>
2021-05-18 20:40:44 +0545

    The canvas is simpler

--- components/editor/Canvas.js
+++ components/editor/Canvas.js
@@ -1,19 +1,10 @@
 import { Box } from '@chakra-ui/layout';
-import { Frame } from '@craftjs/core';
-import { useCallback } from 'react';
-import { useEditorState } from 'store/editor';
+import { Canvas as Canvs } from '@graftini/graft';
 
 export default function Canvas() {
-  const defaultMarkup = useEditorState(
-    useCallback((state) => state.pages[state.currentOpenPage], []),
-    // Only load the markup on startup because craftjs handles the internal state itself.
-    // Make sure the canvas is re-created when page changes.
-    useCallback(() => true, [])
-  );
-
   return (
     <Box sx={{ width: '100%', height: '100vh', boxShadow: 'md', zIndex: 1 }}>
-      <Frame data={defaultMarkup} />
+      <Canvs />
     </Box>
   );
 }
