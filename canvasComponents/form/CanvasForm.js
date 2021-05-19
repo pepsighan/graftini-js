92c18ac870995042c054ba05b5c3133d723edcd9
Sharad Chand <pepsighan@sharadchand.com>
2021-05-19 09:58:42 +0545

    Sync the prop changes

--- canvasComponents/form/CanvasForm.js
+++ canvasComponents/form/CanvasForm.js
@@ -3,9 +3,9 @@ import produce from 'immer';
 import { useMemo } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
-export default function CanvasForm({ elementId, onTransformValues, children }) {
-  const { getState } = useEditor();
-  const componentProps = useMemo(() => getState()[elementId].props, [elementId, getState]);
+export default function CanvasForm({ componentId, onTransformValues, children }) {
+  const { getState, updateComponentProps } = useEditor();
+  const componentProps = useMemo(() => getState()[componentId].props, [componentId, getState]);
 
   const form = useForm({
     defaultValues: componentProps,
@@ -17,18 +17,7 @@ export default function CanvasForm({ elementId, onTransformValues, children }) {
         transformed = produce(values, onTransformValues);
       }
 
-      // TODO: Set the props of the element. There are no API to do it right now
-      // with graft.
-      //
-      // Sync any changes within the form to the component props.
-      // setComponentProps((props) => {
-      //   Object.keys(transformed).forEach((key) => {
-      //     props[key] = transformed[key];
-
-      //     // Sync the transformed values to the form fields itself.
-      //     form.setValue(key, transformed[key]);
-      //   });
-      // });
+      updateComponentProps(componentId, (props) => ({ ...props, transformed }));
 
       // We do not do show any errors here.
       return { values, errors: {} };
