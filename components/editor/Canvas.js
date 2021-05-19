14bff5850375426d2d7c145d9a71884a406a30df
Sharad Chand <pepsighan@sharadchand.com>
2021-05-19 15:09:47 +0545

    Disable user select

--- components/editor/Canvas.js
+++ components/editor/Canvas.js
@@ -3,7 +3,15 @@ import { Canvas as Canvs } from '@graftini/graft';
 
 export default function Canvas() {
   return (
-    <Box sx={{ width: '100%', height: '100vh', boxShadow: 'md', zIndex: 1 }}>
+    <Box
+      sx={{
+        width: '100%',
+        height: '100vh',
+        border: '1px',
+        borderColor: 'gray.300',
+        userSelect: 'none',
+      }}
+    >
       <Canvs />
     </Box>
   );
