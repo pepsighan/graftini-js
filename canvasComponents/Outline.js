c68bb4447fd6db046e9b86a18455a40989a45cf4
Sharad Chand <pepsighan@sharadchand.com>
2021-05-20 13:39:41 +0545

    Show the title of the component

--- canvasComponents/Outline.js
+++ canvasComponents/Outline.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 
-import { useComponentId } from '@graftini/graft';
+import { useComponentId, useEditorState } from '@graftini/graft';
 import { useCallback } from 'react';
 import { useDesignerState } from 'store/designer';
 
@@ -11,12 +11,19 @@ export default function Outline({ children }) {
   );
   const selectComponent = useDesignerState(useCallback((state) => state.selectComponent, []));
 
+  const name = useEditorState(useCallback((state) => state[componentId].props.name, [componentId]));
+
   return (
     <div
       css={{
-        outline: isSelected ? '1px solid black' : null,
+        position: 'relative',
+        outline: isSelected ? '1px solid #9999ff' : null,
+        '& > .component-toolbox': {
+          display: isSelected ? 'block' : 'none',
+        },
+
         '&:hover': {
-          outline: '1px solid black',
+          outline: '1px solid #9999ff',
         },
       }}
       onClick={useCallback(
@@ -27,6 +34,23 @@ export default function Outline({ children }) {
         [componentId, selectComponent]
       )}
     >
+      <div
+        className="component-toolbox"
+        css={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          transform: 'translateY(-100%)',
+          fontSize: 12,
+          paddingLeft: 4,
+          paddingRight: 4,
+          paddingTop: 2,
+          paddingBottom: 2,
+          backgroundColor: '#9999ff',
+        }}
+      >
+        {name || 'Untitled'}
+      </div>
       {children}
     </div>
   );
