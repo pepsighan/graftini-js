2c6d52e28daf251f2206358c8773b44a935e26d0
Sharad Chand <pepsighan@sharadchand.com>
2021-05-23 17:40:32 +0545

    Full width the preview

--- components/preview/Preview.js
+++ components/preview/Preview.js
@@ -21,7 +21,17 @@ export default function Preview({ initialRoute }) {
   const rootNode = componentMap[ROOT_NODE_ID];
 
   return (
-    <IFrame>
+    <IFrame
+      style={{
+        width: '100%',
+        // The height of the nav is substracted, so that the preview does not cause window-wide scroll.
+        height: 'calc(100vh - 49px)',
+        border: '1px',
+        borderColor: 'gray.300',
+        // Any content that overflows vertically will have the scrollbar on this box itself.
+        overflowY: 'auto',
+      }}
+    >
       {() => (
         <>
           {rootNode.childrenNodes.map((componentId) => (
