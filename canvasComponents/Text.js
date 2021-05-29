161e02cd01919ab8fa6179bddcfbeedf2269d308
Sharad Chand <pepsighan@sharadchand.com>
2021-05-29 22:21:43 +0545

    The default tag is p

--- canvasComponents/Text.js
+++ canvasComponents/Text.js
@@ -17,6 +17,7 @@ function Text(props) {
 
 Text.graftOptions = {
   defaultProps: {
+    tag: 'p',
     color: { r: 0, g: 0, b: 0, a: 1 },
     content: 'Lorem ipsum dolor sit amet.',
     fontSize: 16,
