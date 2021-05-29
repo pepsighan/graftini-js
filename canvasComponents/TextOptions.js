146ed1e6fd8d140e92e24f04d417507b22ef600c
Sharad Chand <pepsighan@sharadchand.com>
2021-05-29 22:16:28 +0545

    Label the text options

--- canvasComponents/TextOptions.js
+++ canvasComponents/TextOptions.js
@@ -1,7 +1,9 @@
+import { Grid } from '@chakra-ui/layout';
 import { useCallback } from 'react';
 import { parsePositiveInteger } from 'utils/parser';
 import CanvasForm from './form/CanvasForm';
 import ColorPicker from './form/ColorPicker';
+import Labelled from './form/Labelled';
 import NumberInput from './form/NumberInput';
 import TextInput from './form/TextInput';
 
@@ -13,9 +15,18 @@ export default function TextOptions({ componentId }) {
         values.fontSize = parsePositiveInteger(values.fontSize);
       }, [])}
     >
-      <TextInput name="name" label="Name" />
-      <NumberInput name="fontSize" label="Font Size" spaceTop />
-      <ColorPicker name="color" label="Color" spaceTop />
+      {/* Making a 6 column grid system. */}
+      <Grid templateColumns="repeat(6, minmax(0, 1fr))" alignItems="center" gap={4}>
+        <Labelled label="Name">
+          <TextInput name="name" />
+        </Labelled>
+        <Labelled label="Font Size">
+          <NumberInput name="fontSize" />
+        </Labelled>
+        <Labelled label="Color">
+          <ColorPicker name="color" />
+        </Labelled>
+      </Grid>
     </CanvasForm>
   );
 }
