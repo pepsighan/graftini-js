41fe08443c4c38005fe438b5fce9aa6d2868d442
Sharad Chand <pepsighan@sharadchand.com>
2021-05-30 19:06:32 +0545

    Position the inputs in their rightful location

--- canvasComponents/form/SpacingField.js
+++ canvasComponents/form/SpacingField.js
@@ -1,18 +1,22 @@
-import { Box, Flex, Input } from '@chakra-ui/react';
+import { Grid, GridItem, Input } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 
 export default function SpacingField({ name }) {
   const { register } = useFormContext();
   return (
-    <Box>
-      <Flex>
+    <Grid templateColumns="repeat(3, minmax(0, 1fr))">
+      <GridItem colStart={2} colEnd={3}>
         <Input size="sm" bg="white" {...register(`${name}.top`)} autoComplete="off" />
+      </GridItem>
+      <GridItem colStart={1} colEnd={2}>
         <Input size="sm" bg="white" {...register(`${name}.right`)} autoComplete="off" />
-      </Flex>
-      <Flex mt={1}>
+      </GridItem>
+      <GridItem colStart={3}>
         <Input size="sm" bg="white" {...register(`${name}.left`)} autoComplete="off" />
+      </GridItem>
+      <GridItem colStart={2} colEnd={3}>
         <Input size="sm" bg="white" {...register(`${name}.bottom`)} autoComplete="off" />
-      </Flex>
-    </Box>
+      </GridItem>
+    </Grid>
   );
 }
