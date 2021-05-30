f751892003cc74350cbe568b8dcc011cb24a2f48
Sharad Chand <pepsighan@sharadchand.com>
2021-05-30 14:09:29 +0545

    Show the select options for unit

--- canvasComponents/form/FontSize.js
+++ canvasComponents/form/FontSize.js
@@ -1,5 +1,6 @@
 import { Input } from '@chakra-ui/input';
 import { Flex } from '@chakra-ui/layout';
+import { Select } from '@chakra-ui/select';
 import { useFormContext } from 'react-hook-form';
 
 export default function FontSize({ name }) {
@@ -12,9 +13,12 @@ export default function FontSize({ name }) {
         size="sm"
         bg="white"
         autoComplete="off"
-        flex={3}
+        flex={1}
       />
-      <Input {...register(`${name}.unit`)} size="sm" bg="white" autoComplete="off" flex={1} />
+      <Select {...register(`${name}.unit`)} size="sm" bg="white" autoComplete="off" flex={1}>
+        <option value="px">px</option>
+        <option value="rem">rem</option>
+      </Select>
     </Flex>
   );
 }
