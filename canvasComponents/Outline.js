777cede48cb06887212fd4608cb8b465796e2280
Sharad Chand <pepsighan@sharadchand.com>
2021-05-31 14:06:26 +0545

    Show the name badge

--- canvasComponents/Outline.js
+++ canvasComponents/Outline.js
@@ -12,9 +12,9 @@ export default function Outline({ componentRef }) {
     useCallback((state) => state.selectedComponentId === componentId, [componentId])
   );
 
-  return (
-    <>{isSelected && <ActualOutline componentId={componentId} componentRef={componentRef} />}</>
-  );
+  return isSelected ? (
+    <ActualOutline componentId={componentId} componentRef={componentRef} />
+  ) : null;
 }
 
 function ActualOutline({ componentId, componentRef }) {
@@ -25,16 +25,37 @@ function ActualOutline({ componentId, componentRef }) {
   return (
     <Portal>
       {dimensions && (
-        <div
-          css={{
-            position: 'fixed',
-            top: y,
-            left: x,
-            width,
-            height,
-            border: `2px solid ${theme.colors.primary[300]}`,
-          }}
-        />
+        <>
+          {/* This is the panel on which options of the components are show. */}
+          <div
+            css={{
+              display: 'flex',
+              alignItems: 'center',
+              position: 'fixed',
+              // If it overflows from the top, then show it to the bottom of the component.
+              top: y - 18 >= 0 ? y - 18 : y + height,
+              left: x,
+              backgroundColor: theme.colors.primary[300],
+              fontSize: 12,
+              height: 18,
+              paddingLeft: 8,
+              paddingRight: 8,
+            }}
+          >
+            {name ?? 'Untitled'}
+          </div>
+          <div
+            css={{
+              position: 'fixed',
+              top: y,
+              left: x,
+              width,
+              height,
+              border: `2px solid ${theme.colors.primary[300]}`,
+              pointerEvents: 'none',
+            }}
+          />
+        </>
       )}
     </Portal>
   );
