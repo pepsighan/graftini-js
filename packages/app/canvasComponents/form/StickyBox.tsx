119d8c7a877ed90def8b015603ec30e9de0fa56d
Sharad Chand <pepsighan@sharadchand.com>
2021-06-02 18:10:35 +0545

    Do not let the item overflow the bottom

--- packages/app/canvasComponents/form/StickyBox.tsx
+++ packages/app/canvasComponents/form/StickyBox.tsx
@@ -2,6 +2,7 @@ import { Box, BoxProps } from '@chakra-ui/layout';
 import { rightSidebarWidth } from 'components/editor/RightSidebar';
 import { useDimensions } from 'hooks/useDimensions';
 import { MutableRefObject } from 'react';
+import { useWindowSize } from 'react-use';
 
 type StickBoxProps = {
   stickToRef: MutableRefObject<HTMLElement>;
@@ -10,10 +11,15 @@ type StickBoxProps = {
 
 export default function StickyBox({ stickToRef, heightOfContent, ...rest }: StickBoxProps) {
   const stickToDim = useDimensions(stickToRef);
+  const { height } = useWindowSize();
+
+  const boxBottom = stickToDim.top + heightOfContent;
+  const bottom = boxBottom > height ? 10 : boxBottom - height;
+
   return (
     <Box
       position="fixed"
-      top={(stickToDim?.top ?? 0) - heightOfContent}
+      bottom={bottom}
       right={rightSidebarWidth + 10}
       zIndex="popover"
       {...rest}
