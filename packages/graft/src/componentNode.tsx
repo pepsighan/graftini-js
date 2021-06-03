0c5869770a68ff6e48e4ae018a775768a672a164
Sharad Chand <pepsighan@sharadchand.com>
2021-06-03 15:08:30 +0545

    Do not show marker within the canvas

--- packages/graft/src/componentNode.tsx
+++ packages/graft/src/componentNode.tsx
@@ -2,8 +2,7 @@ import React, { ReactNode, useCallback } from 'react';
 import { CanvasContext, ComponentContext, useComponentId } from './context';
 import { useOnComponentDrag } from './drag';
 import { useIdentifyCurrentDropLocation } from './dropLocation';
-import { DropMarker, DropMarkerCanvasEmpty } from './dropMarker';
-import { GraftComponent, GraftComponentProps, useResolver } from './resolver';
+import { GraftComponent, useResolver } from './resolver';
 import { ComponentProps, useEditorStateInternal } from './schema';
 
 type ComponentNodeProps = {
@@ -42,30 +41,12 @@ export function ComponentNode({ componentId, isRoot }: ComponentNodeProps) {
         <CanvasContext.Provider value={componentId}>
           <ComponentContext.Provider value={componentId}>
             <ComponentWrapper component={Component} componentProps={componentProps}>
-              {/* When there are no children, and there are no children here. Show a marker 
-              in the insides. This will help is marking a component which is also a canvas.*/}
-              {childrenNodes.length === 0 && <DropMarkerCanvasEmpty />}
-
               {childrenNodes.map((componentId) => (
                 <ComponentNode key={componentId} componentId={componentId} />
               ))}
             </ComponentWrapper>
           </ComponentContext.Provider>
         </CanvasContext.Provider>
-
-        {/* Show a drop marker when a canvas is hovered over after it. All of the canvas after components will
-        become activated when there is an ongoing drag operation.
-        
-        Also, if this is a root component then nothing after it can be added. Only children can be added. */}
-        {!isRoot && (
-          // Provided the component Id here so that the dropped item is added after this component but for the
-          // parent canvas.
-          <ComponentContext.Provider value={componentId}>
-            <ComponentWrapper component={CanvasAfter} componentProps={{}}>
-              <DropMarker />
-            </ComponentWrapper>
-          </ComponentContext.Provider>
-        )}
       </>
     );
   }
@@ -73,8 +54,6 @@ export function ComponentNode({ componentId, isRoot }: ComponentNodeProps) {
   return (
     <ComponentContext.Provider value={componentId}>
       <ComponentWrapper component={Component} componentProps={componentProps} />
-      {/* This is a drop marker to position the drop location just after the component. */}
-      <DropMarker />
     </ComponentContext.Provider>
   );
 }
@@ -113,33 +92,5 @@ function ComponentWrapper({
     >
       {children}
     </Component>
-  );
-}
-
-/**
- * A container after the canvas which enables a drop location to add component after it.
- * This won't be visible until a drag operation is active.
- */
-function CanvasAfter({ onDragOver, onDragLeave, children }: GraftComponentProps) {
-  const componentId = useComponentId();
-
-  const isDragging = useEditorStateInternal(
-    useCallback(
-      (state) =>
-        state.draggedOver.isDragging &&
-        // The component cannot be dropped after itself. It has no meaning really.
-        state.draggedOver.component?.id !== componentId,
-      [componentId]
-    )
-  );
-
-  return (
-    <div
-      style={{ width: '100%', height: isDragging ? 100 : 0 }}
-      onDragOver={onDragOver}
-      onDragLeave={onDragLeave}
-    >
-      {children}
-    </div>
   );
 }
