d18f645bb05f8836cfe646f8f3cc82a8eb2768b6
Sharad Chand <pepsighan@sharadchand.com>
2021-06-04 18:24:15 +0545

    Reduce a bit of jerk

--- packages/graft/src/preview.tsx
+++ packages/graft/src/preview.tsx
@@ -1,4 +1,4 @@
-import { motion, useMotionValue } from 'framer-motion';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { useResolver } from './resolver';
 import { DraggedOver, DraggingState, useEditorStoreApiInternal } from './schema';
@@ -22,7 +22,16 @@ export function DragPreview({ correction }: DragPreviewProps) {
   const { subscribe } = useEditorStoreApiInternal();
   const posX = useMotionValue(0);
   const posY = useMotionValue(0);
+  const opacity = useMotionValue(0);
   const [component, setComponent] = useState<string | null>(null);
+
+  // A spring is used so that there is less abrupt changes in the position. This
+  // can happen when going between the boundaries of iframe.
+  // TODO: Fix the abrupt position change when going in and out of iframe. This happens
+  // because the cursorPosition and isDragging state is not updated at the same time.
+  // For now, this works.
+  const x = useSpring(posX, { stiffness: 300, damping: 20, mass: 0.1 });
+  const y = useSpring(posY, { stiffness: 300, damping: 20, mass: 0.1 });
 
   useEffect(() => {
     return subscribe(
@@ -33,16 +42,20 @@ export function DragPreview({ correction }: DragPreviewProps) {
           posX.set(draggedOver.cursorPosition.x + (isInCanvas ? correction?.x ?? 0 : 0));
           posY.set(draggedOver.cursorPosition.y + (isInCanvas ? correction?.y ?? 0 : 0));
           setComponent(draggedOver.component!.type);
+
+          // Do not show the item coming from the origin.
+          setTimeout(() => opacity.set(1), 200);
           return;
         }
 
+        opacity.set(0);
         posX.set(0);
         posY.set(0);
         setComponent(null);
       },
       (state) => state.draggedOver
     );
-  }, [correction?.x, correction?.y, posX, posY, subscribe]);
+  }, [correction?.x, correction?.y, opacity, posX, posY, subscribe]);
 
   return (
     <>
@@ -51,10 +64,11 @@ export function DragPreview({ correction }: DragPreviewProps) {
           layout
           style={{
             position: 'fixed',
+            opacity,
             top: 0,
             left: 0,
-            x: posX,
-            y: posY,
+            x,
+            y,
             pointerEvents: 'none',
           }}
         >
