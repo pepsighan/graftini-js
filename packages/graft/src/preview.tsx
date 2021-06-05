4fcf65214842ac6074d34cba4b63de123be3cdce
Sharad Chand <pepsighan@sharadchand.com>
2021-06-05 21:44:45 +0545

    No need for spring

--- packages/graft/src/preview.tsx
+++ packages/graft/src/preview.tsx
@@ -1,4 +1,4 @@
-import { motion, useMotionValue, useSpring } from 'framer-motion';
+import { motion, useMotionValue } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { useResolver } from './resolver';
 import { DraggedOver, DraggingState, useEditorStoreApiInternal } from './schema';
@@ -24,14 +24,6 @@ export function DragPreview({ correction }: DragPreviewProps) {
   const posY = useMotionValue(0);
   const opacity = useMotionValue(0);
   const [component, setComponent] = useState<string | null>(null);
-
-  // A spring is used so that there is less abrupt changes in the position. This
-  // can happen when going between the boundaries of iframe.
-  // TODO: Fix the abrupt position change when going in and out of iframe. This happens
-  // because the cursorPosition and isDragging state is not updated at the same time.
-  // For now, this works.
-  const x = useSpring(posX, { stiffness: 300, damping: 20, mass: 0.1 });
-  const y = useSpring(posY, { stiffness: 300, damping: 20, mass: 0.1 });
 
   useEffect(() => {
     return subscribe(
@@ -67,8 +59,8 @@ export function DragPreview({ correction }: DragPreviewProps) {
             opacity,
             top: 0,
             left: 0,
-            x,
-            y,
+            x: posX,
+            y: posY,
             pointerEvents: 'none',
           }}
         >
