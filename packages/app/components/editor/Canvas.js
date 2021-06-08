ab9ac24d1b3528e1fa7829c8b4d3610b6efdd908
Sharad Chand <pepsighan@sharadchand.com>
2021-06-08 18:48:36 +0545

    Will do root selection in the root override

--- packages/app/components/editor/Canvas.js
+++ packages/app/components/editor/Canvas.js
@@ -1,18 +1,9 @@
 import IFrame from 'components/IFrame';
-import { Canvas as Canvs, DropMarker, ROOT_NODE_ID } from 'graft';
-import { forwardRef, useCallback } from 'react';
-import { useDesignerState } from 'store/designer';
+import { Canvas as Canvs, DropMarker } from 'graft';
+import { forwardRef } from 'react';
 import theme from 'utils/theme';
 
 const Canvas = forwardRef((_, ref) => {
-  const selectComponent = useDesignerState(useCallback((state) => state.selectComponent, []));
-
-  const onRootSelection = useCallback(() => {
-    // The selection is not actually the root node. But since the root node is directly below.
-    // Its the technically the same.
-    selectComponent(ROOT_NODE_ID);
-  }, [selectComponent]);
-
   return (
     <div
       ref={ref}
@@ -26,15 +17,10 @@ const Canvas = forwardRef((_, ref) => {
         style={{
           width: '100%',
           height: '100%',
-          border: '1px',
-          borderColor: 'gray.300',
-          // Any content that overflows vertically will have the scrollbar on this box itself.
-          overflowY: 'auto',
         }}
       >
         {() => (
           <div
-            onClick={onRootSelection}
             style={{
               width: '100%',
               height: '100vh',
