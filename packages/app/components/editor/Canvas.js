edfa974d5bc06ef91cdd37f5a2cb01d3520f3420
Sharad Chand <pepsighan@sharadchand.com>
2021-06-08 18:16:58 +0545

    Correct the positions due to iframe

--- packages/app/components/editor/Canvas.js
+++ packages/app/components/editor/Canvas.js
@@ -1,9 +1,10 @@
 import IFrame from 'components/IFrame';
-import { Canvas as Canvs, ROOT_NODE_ID } from 'graft';
-import { useCallback } from 'react';
+import { Canvas as Canvs, DropMarker, ROOT_NODE_ID } from 'graft';
+import { forwardRef, useCallback } from 'react';
 import { useDesignerState } from 'store/designer';
+import theme from 'utils/theme';
 
-export default function Canvas() {
+const Canvas = forwardRef((_, ref) => {
   const selectComponent = useDesignerState(useCallback((state) => state.selectComponent, []));
 
   const onRootSelection = useCallback(() => {
@@ -13,29 +14,40 @@ export default function Canvas() {
   }, [selectComponent]);
 
   return (
-    <IFrame
-      title="Designer"
+    <div
+      ref={ref}
       style={{
         width: '100%',
         height: '100%',
-        border: '1px',
-        borderColor: 'gray.300',
-        // Any content that overflows vertically will have the scrollbar on this box itself.
-        overflowY: 'auto',
       }}
     >
-      {() => (
-        <div
-          onClick={onRootSelection}
-          style={{
-            width: '100%',
-            height: '100vh',
-            userSelect: 'none',
-          }}
-        >
-          <Canvs />
-        </div>
-      )}
-    </IFrame>
+      <IFrame
+        title="Designer"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: '1px',
+          borderColor: 'gray.300',
+          // Any content that overflows vertically will have the scrollbar on this box itself.
+          overflowY: 'auto',
+        }}
+      >
+        {() => (
+          <div
+            onClick={onRootSelection}
+            style={{
+              width: '100%',
+              height: '100vh',
+              userSelect: 'none',
+            }}
+          >
+            <Canvs />
+            <DropMarker color={theme.colors.primary[500]} />
+          </div>
+        )}
+      </IFrame>
+    </div>
   );
-}
+});
+
+export default Canvas;
