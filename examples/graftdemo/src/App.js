5654d52144a267f8623978445cb6e2a3251a85d3
Sharad Chand <pepsighan@sharadchand.com>
2021-06-09 14:06:06 +0545

    No gonna need the smaller container

--- examples/graftdemo/src/App.js
+++ examples/graftdemo/src/App.js
@@ -3,19 +3,19 @@ import { Box, Text as Txt } from 'bricks';
 import {
   Canvas,
   DragPreview,
+  DrawMarker,
   DropMarker,
   Editor,
   useComponentId,
   useCreateComponent,
   useEditorState,
-  DrawMarker,
 } from 'graft';
 import { forwardRef, useCallback } from 'react';
 import IFrame from './IFrame';
 
 export default function App() {
   return (
-    <Editor resolvers={{ Container, SmallerContainer, Text }} iframeCorrection={{ x: 0, y: 70 }}>
+    <Editor resolvers={{ Container, Text }} iframeCorrection={{ x: 0, y: 70 }}>
       <Menu />
       <IFrame
         style={{
@@ -64,16 +64,6 @@ function Menu() {
       >
         Container
       </button>
-      <button
-        {...useCreateComponent({
-          type: 'SmallerContainer',
-          childAppendDirection: 'horizontal',
-          isCanvas: true,
-        })}
-        style={{ padding: 16, marginLeft: 16 }}
-      >
-        Smaller Container
-      </button>
       <button {...useCreateComponent({ type: 'Text' })} style={{ padding: 16, marginLeft: 16 }}>
         Text
       </button>
@@ -120,53 +110,6 @@ const Container = forwardRef(({ children, ...rest }, ref) => {
 });
 
 Container.graftOptions = {
-  preview: () => (
-    <Box
-      width={{ size: 100, unit: 'px' }}
-      height={{ size: 100, unit: 'px' }}
-      color={{ r: 100, g: 100, b: 200 }}
-    />
-  ),
-};
-
-const SmallerContainer = forwardRef(({ children, ...rest }, ref) => {
-  const id = useComponentId();
-  const noChildren = useEditorState(
-    useCallback((state) => state[id].childrenNodes.length === 0, [id])
-  );
-
-  const border = { color: { r: 0, g: 0, b: 0 }, style: 'solid', width: 1 };
-
-  return (
-    <Box
-      ref={ref}
-      {...rest}
-      border={{
-        left: border,
-        right: border,
-        top: border,
-        bottom: border,
-      }}
-      color={{ r: 100, g: 100, b: 230, a: 0.3 }}
-      width={{ size: 200, unit: 'px' }}
-      height={
-        noChildren
-          ? {
-              size: 180,
-              unit: 'px',
-            }
-          : 'auto'
-      }
-      padding={{ top: 8, left: 8, right: 8, bottom: 8 }}
-      position="relative"
-    >
-      <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }}>{id}</div>
-      {children}
-    </Box>
-  );
-});
-
-SmallerContainer.graftOptions = {
   preview: () => (
     <Box
       width={{ size: 100, unit: 'px' }}
