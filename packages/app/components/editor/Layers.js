2ab789e56a3808ade88bed3b74b8de91f5aa6c0b
Sharad Chand <pepsighan@sharadchand.com>
2021-06-14 18:01:37 +0545

    Render the layers

---
+++ packages/app/components/editor/Layers.js
@@ -0,0 +1,41 @@
+import { Box, Button, Text } from '@chakra-ui/react';
+import { useCallback } from 'react';
+import { useDesignerState } from 'store/designer';
+import { Tree } from 'tread';
+
+export default function Layers() {
+  const componentMap = useDesignerState(
+    useCallback((state) => state.pages[state.currentOpenPage], [])
+  );
+
+  return (
+    <Box mt={8}>
+      <Text as="span" fontSize="sm" fontWeight="bold">
+        Layers
+      </Text>
+
+      <Box mt={2}>
+        <Tree tree={componentMap} renderItem={LayerItem} renderSubTree={SubTree} />
+      </Box>
+    </Box>
+  );
+}
+
+function LayerItem({ item }) {
+  return (
+    <Button
+      fontSize="sm"
+      size="sm"
+      fontWeight="normal"
+      display="block"
+      isFullWidth
+      textAlign="left"
+    >
+      {!item.parentId ? 'Root' : item.props.name || 'Untitled'}
+    </Button>
+  );
+}
+
+function SubTree({ children }) {
+  return <Box ml={4}>{children}</Box>;
+}
--- packages/app/components/editor/LeftSidebar.js
+++ packages/app/components/editor/LeftSidebar.js
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/layout';
+import Layers from './Layers';
 import Pages from './Pages';
 
 export default function LeftSidebar() {
   return (
     <Box py={4} px={3} width={300} bg="gray.100" borderRight="1px" borderRightColor="gray.400">
       <Pages />
+      <Layers />
     </Box>
   );
 }
