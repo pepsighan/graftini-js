13bbdc5eef5d1885523862bace21a185950b26fe
Sharad Chand <pepsighan@sharadchand.com>
2021-06-19 13:53:06 +0545

    Use pan to update the size

--- packages/app/components/editor/ResizableFrame.js
+++ packages/app/components/editor/ResizableFrame.js
@@ -4,8 +4,8 @@ import { useEditor } from 'graft';
 import { useCallback } from 'react';
 import { useDesignerState } from 'store/designer';
 
-export default function ResizeableFrame(props) {
-  const { top, left, right, bottom } = useSidePositions(props);
+export default function ResizeableFrame({ componentId, ...rest }) {
+  const { top, left, right, bottom } = useSidePositions(rest);
 
   const isBoxResizing = useDesignerState(useCallback((state) => state.isBoxResizing, []));
   const setIsBoxResizing = useDesignerState(useCallback((state) => state.setIsBoxResizing, []));
@@ -42,6 +42,8 @@ export default function ResizeableFrame(props) {
       {/* The top side. */}
       <FrameSide
         {...top}
+        componentId={componentId}
+        original={rest}
         cursor="row-resize"
         type="top"
         onPointerDown={onStartResizing}
@@ -50,6 +52,8 @@ export default function ResizeableFrame(props) {
       {/* The right side. */}
       <FrameSide
         {...right}
+        componentId={componentId}
+        original={rest}
         cursor="col-resize"
         type="right"
         onPointerDown={onStartResizing}
@@ -58,6 +62,8 @@ export default function ResizeableFrame(props) {
       {/* The bottom side. */}
       <FrameSide
         {...bottom}
+        componentId={componentId}
+        original={rest}
         cursor="row-resize"
         type="bottom"
         onPointerDown={onStartResizing}
@@ -66,6 +72,8 @@ export default function ResizeableFrame(props) {
       {/* The left side. */}
       <FrameSide
         {...left}
+        componentId={componentId}
+        original={rest}
         cursor="col-resize"
         type="left"
         onPointerDown={onStartResizing}
@@ -75,25 +83,43 @@ export default function ResizeableFrame(props) {
   );
 }
 
-function FrameSide({ x, y, width, height, cursor, type, onPointerDown, onPointerUp }) {
+function FrameSide({
+  x,
+  y,
+  width,
+  height,
+  cursor,
+  type,
+  onPointerDown,
+  onPointerUp,
+  componentId,
+  original,
+}) {
+  const { updateWidth, updateHeight } = useDimensionUpdate({ componentId });
+
   return (
     <motion.div
       onPointerDown={onPointerDown}
       onPointerUp={onPointerUp}
-      onPan={(_, pointInfo) => {
-        let diff = 0;
-        if (type === 'left') {
-          diff = -pointInfo.offset.x;
-        } else if (type === 'right') {
-          diff = pointInfo.offset.x;
-        } else if (type === 'top') {
-          diff = -pointInfo.offset.y;
-        } else if (type === 'bottom') {
-          diff = pointInfo.offset.y;
-        }
-
-        console.log(diff);
-      }}
+      onPan={useCallback(
+        (_, pointInfo) => {
+          console.log(pointInfo);
+          if (type === 'left') {
+            const diffW = -pointInfo.offset.x;
+            updateWidth(original.width.get() + diffW);
+          } else if (type === 'right') {
+            const diffW = pointInfo.offset.x;
+            updateWidth(original.width.get() + diffW);
+          } else if (type === 'top') {
+            const diffH = -pointInfo.offset.y;
+            updateHeight(original.height.get() + diffH);
+          } else if (type === 'bottom') {
+            const diffH = pointInfo.offset.y;
+            updateHeight(original.height.get() + diffH);
+          }
+        },
+        [original.height, original.width, type, updateHeight, updateWidth]
+      )}
       style={{
         position: 'fixed',
         top: 0,
@@ -120,7 +146,7 @@ function useSidePositions(props) {
   return { top, right, bottom, left };
 }
 
-function useTopSide({ posX, posY, width, height, componentId }) {
+function useTopSide({ posX, posY, width }) {
   const topX = useTransform(posX, (x) => x + frameWidth);
   const topY = useTransform(posY, (y) => y);
   const topWidth = useTransform(width, (w) => w - frameWidth * 2);
@@ -128,7 +154,7 @@ function useTopSide({ posX, posY, width, height, componentId }) {
   return { x: topX, y: topY, width: topWidth, height: frameWidth };
 }
 
-function useRightSide({ posX, posY, width, height, componentId }) {
+function useRightSide({ posX, posY, width, height }) {
   const rightX = useTransform([posX, width], ([x, w]) => x + w - frameWidth);
   const rightY = useTransform(posY, (y) => y + frameWidth);
   const rightHeight = useTransform(height, (h) => h - frameWidth * 2);
@@ -136,7 +162,7 @@ function useRightSide({ posX, posY, width, height, componentId }) {
   return { x: rightX, y: rightY, width: frameWidth, height: rightHeight };
 }
 
-function useBottomSide({ posX, posY, width, height, componentId }) {
+function useBottomSide({ posX, posY, width, height }) {
   const bottomX = useTransform(posX, (x) => x + frameWidth);
   const bottomY = useTransform([posY, height], ([y, h]) => y + h - frameWidth);
   const bottomWidth = useTransform(width, (w) => w - frameWidth * 2);
@@ -144,7 +170,7 @@ function useBottomSide({ posX, posY, width, height, componentId }) {
   return { x: bottomX, y: bottomY, width: bottomWidth, height: frameWidth };
 }
 
-function useLeftSide({ posX, posY, width, height, componentId }) {
+function useLeftSide({ posX, posY, height }) {
   const leftX = useTransform(posX, (x) => x);
   const leftY = useTransform(posY, (y) => y + frameWidth);
   const leftHeight = useTransform(height, (h) => h - frameWidth * 2);
