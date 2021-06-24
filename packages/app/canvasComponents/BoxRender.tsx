2505fffd9592223e4021ca379a0ebc3be093eb33
Sharad Chand <pepsighan@sharadchand.com>
2021-06-24 16:38:38 +0545

    No transformation needed

--- packages/app/canvasComponents/BoxRender.tsx
+++ packages/app/canvasComponents/BoxRender.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@graftini/bricks';
-import { useBoxTransformedProps } from 'hooks/useBoxTransformedProps';
 import { BoxComponentProps } from './Box';
 
 export default function BoxRender(props: BoxComponentProps) {
-  return <Box {...useBoxTransformedProps(props)} />;
+  return <Box {...props} />;
 }
