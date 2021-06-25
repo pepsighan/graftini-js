c311519257422d284b18f0b4b911daa2042a4aa7
Sharad Chand <pepsighan@sharadchand.com>
2021-06-25 22:30:33 +0545

    Hide scrollbars on the right sidebar

--- packages/app/components/editor/RightSidebar.js
+++ packages/app/components/editor/RightSidebar.js
@@ -19,6 +19,16 @@ export default function RightSidebar() {
       px={3}
       borderLeft="1px"
       borderLeftColor="gray.400"
+      sx={{
+        // Hide scrollbars on all browsers.
+        // https://stackoverflow.com/a/49278385
+        scrollbarWidth: 'none',
+        '-ms-overflow-style': 'none',
+        '&::-webkit-scrollbar': {
+          width: 0,
+          height: 0,
+        },
+      }}
     >
       <ButtonGroup display="flex" size="xs" mb={4}>
         <Button flex={1} isActive={index === 0} onClick={onClick(0)}>
