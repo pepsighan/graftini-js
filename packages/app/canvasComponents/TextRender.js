aa8f1d8d972cb2356c0122026a33a2d8dd24a515
Sharad Chand <pepsighan@sharadchand.com>
2021-06-25 12:23:43 +0545

    Text is now same as render

--- packages/app/canvasComponents/TextRender.js
+++
@@ -1,9 +0,0 @@
-import { Text, TextBody } from '@graftini/bricks';
-
-export default function TextRender({ text, ...rest }) {
-  return (
-    <Text {...rest}>
-      <TextBody content={text} />
-    </Text>
-  );
-}
