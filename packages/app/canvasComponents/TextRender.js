66e293a2f14fddba365290e62ffa6dd0d0fbb919
Sharad Chand <pepsighan@sharadchand.com>
2021-06-25 12:05:38 +0545

    Extra the text rendering login outside

--- packages/app/canvasComponents/TextRender.js
+++ packages/app/canvasComponents/TextRender.js
@@ -1,50 +1,9 @@
-import { Text } from '@graftini/bricks';
-import { Text as Txt } from 'slate';
+import { Text, TextBody } from '@graftini/bricks';
 
 export default function TextRender({ text, ...rest }) {
   return (
     <Text {...rest}>
-      {text.map((block, index) => (
-        <Block key={index} node={block} />
-      ))}
+      <TextBody content={text} />
     </Text>
   );
-}
-
-function Block({ node }) {
-  if (Txt.isText(node)) {
-    let text = node.text;
-    if (node.bold) {
-      text = (
-        <Text tag="strong" displayInline fontWeight={600}>
-          {text}
-        </Text>
-      );
-    }
-    // TODO: Reset the text style to not have anything and provide italics and underline
-    // via the Text component.
-    if (node.italic) {
-      text = (
-        <Text tag="em" displayInline>
-          {text}
-        </Text>
-      );
-    }
-    if (node.underlined) {
-      text = (
-        <Text tag="u" displayInline>
-          {text}
-        </Text>
-      );
-    }
-    return text;
-  }
-
-  const children = node.children.map((n) => <Block node={n} />);
-  switch (node.type) {
-    case 'paragraph':
-      return <p>{children}</p>;
-    default:
-      return children;
-  }
 }
