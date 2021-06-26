9ef1c1a2534d4ce8c53c4a3c9c56d2d3d880dbec
Sharad Chand <pepsighan@sharadchand.com>
2021-06-26 20:52:53 +0545

    Inject project id to the preview

--- packages/app/components/preview/Preview.js
+++ packages/app/components/preview/Preview.js
@@ -3,6 +3,7 @@ import { rgbaToCss } from '@graftini/bricks';
 import { ROOT_NODE_ID } from '@graftini/graft';
 import IFrame from 'components/IFrame';
 import useMyProjectFromRouter from 'hooks/useMyProjectFromRouter';
+import { ProjectIdProvider } from 'hooks/useProjectId';
 import NotFound from 'pages/404';
 import { useMemo } from 'react';
 import { parseComponentMap } from 'store/designer';
@@ -23,39 +24,41 @@ export default function Preview({ initialRoute }) {
   const rootNode = componentMap[ROOT_NODE_ID];
 
   return (
-    <IFrame
-      title="Preview"
-      style={{
-        width: '100%',
-        // The height of the nav is substracted, so that the preview does not cause window-wide scroll.
-        height: 'calc(100vh - 40px)',
-        border: '1px',
-        borderColor: 'gray.300',
-        // Any content that overflows vertically will have the scrollbar on this box itself.
-        overflowY: 'auto',
-      }}
-    >
-      {() => (
-        <>
-          {rootNode.props.color && (
-            <Global
-              styles={`
+    <ProjectIdProvider value={project.id}>
+      <IFrame
+        title="Preview"
+        style={{
+          width: '100%',
+          // The height of the nav is substracted, so that the preview does not cause window-wide scroll.
+          height: 'calc(100vh - 40px)',
+          border: '1px',
+          borderColor: 'gray.300',
+          // Any content that overflows vertically will have the scrollbar on this box itself.
+          overflowY: 'auto',
+        }}
+      >
+        {() => (
+          <>
+            {rootNode.props.color && (
+              <Global
+                styles={`
                 body {
                   background-color: ${rgbaToCss(rootNode.props.color)};
                 }
               `}
-            />
-          )}
+              />
+            )}
 
-          {rootNode.childrenNodes.map((componentId) => (
-            <ComponentRender
-              key={componentId}
-              componentId={componentId}
-              componentMap={componentMap}
-            />
-          ))}
-        </>
-      )}
-    </IFrame>
+            {rootNode.childrenNodes.map((componentId) => (
+              <ComponentRender
+                key={componentId}
+                componentId={componentId}
+                componentMap={componentMap}
+              />
+            ))}
+          </>
+        )}
+      </IFrame>
+    </ProjectIdProvider>
   );
 }
