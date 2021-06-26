a79e27c276b177971ce6c1a705c099f71ac7dd25
Sharad Chand <pepsighan@sharadchand.com>
2021-06-26 17:01:32 +0545

    Impl the restricting opacity input

--- packages/app/canvasComponents/form/OpacityInput.js
+++ packages/app/canvasComponents/form/OpacityInput.js
@@ -1,8 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { useCallback } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 export default function OpacityInput({ name }) {
-  const { register } = useFormContext();
+  const { register, setValue, getValues } = useFormContext();
+
+  // Creating the form with register and manual onChange is the right balance
+  // to implement a restricting 0 <= opacity <= 1 input.
+  const { onChange, onBlur, ...rest } = register(name);
+
+  const onInputChange = useCallback(
+    (event) => {
+      const num = event.target.value !== '' ? parseFloat(event.target.value) : 0;
+      if (num <= 0) {
+        setValue(name, 0, { shouldDirty: true, shouldValidate: true });
+      } else if (num >= 1) {
+        setValue(name, 1, { shouldDirty: true, shouldValidate: true });
+      } else if (isNaN(num)) {
+        // If invalid number provided, use the older one.
+        setValue(name, getValues()[name]);
+      } else {
+        setValue(name, parseFloat(num.toFixed(2)), { shouldDirty: true, shouldValidate: true });
+      }
+    },
+    [getValues, name, setValue]
+  );
 
   return (
     <InputGroup>
@@ -18,13 +40,15 @@ export default function OpacityInput({ name }) {
         Opacity
       </InputLeftElement>
       <Input
-        {...register(name)}
+        {...rest}
+        onChange={onInputChange}
         type="number"
         size="sm"
         bg="white"
         autoComplete="off"
         textAlign="right"
         pb="1px" // Align the input text with the label.
+        pr={4} // So that the cursor is visible on the right.
         sx={{
           paddingInlineStart: 14,
         }}
