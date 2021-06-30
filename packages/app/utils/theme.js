caabf7ffa74c297c0fd4b5053c0513bbd19d1d62
Sharad Chand <pepsighan@sharadchand.com>
2021-06-30 22:03:01 +0545

    Use a lighter primary color

--- packages/app/utils/theme.js
+++ packages/app/utils/theme.js
@@ -1,22 +1,8 @@
 import { colors, createTheme } from '@material-ui/core';
 
-const blue = {
-  ...colors.indigo,
-  50: '#b780ff',
-  100: '#a966ff',
-  200: '#9a4dff',
-  300: '#8c33ff',
-  400: '#7d1aff',
-  500: '#6f00ff',
-  600: '#6400e6',
-  700: '#5900cc',
-  800: '#4e00b3',
-  900: '#430099',
-};
-
 export default createTheme({
   palette: {
-    primary: blue,
+    primary: colors.blue,
   },
   typography: {
     fontSize: 14,
