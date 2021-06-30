33c74bfbaf93631c75e1d7e857ed3dc30240f8d7
Sharad Chand <pepsighan@sharadchand.com>
2021-06-30 23:23:52 +0545

    No need for action bar. It shows duplicate info

--- packages/app/components/editor/Selection.js
+++ packages/app/components/editor/Selection.js
@@ -1,9 +1,7 @@
 import { useComponentRegionStoreApi, useRootScrollStoreApi } from '@graftini/graft';
-import { useTheme } from '@material-ui/core';
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { useMotionValue, useTransform } from 'framer-motion';
 import { useCallback, useEffect, useState } from 'react';
 import { useDesignerState } from 'store/designer';
-import ActionBar from './ActionBar';
 import PlainOutline from './PlainOutline';
 import ResizeableFrame from './ResizableFrame';
 
@@ -24,34 +22,16 @@ function ActualSelection({ componentId }) {
   const posY = useMotionValue(0);
   const width = useMotionValue(0);
   const height = useMotionValue(0);
-  const actionPosY = useMotionValue(0);
 
   const { scrollX, scrollY } = useScrollPosition();
 
   const actualX = useTransform([posX, scrollX], subtractScroll);
   const actualY = useTransform([posY, scrollY], subtractScroll);
-  const actualActionPosY = useTransform([actionPosY, scrollY], subtractScroll);
 
-  const isShown = useSelection({ componentId, posX, posY, width, height, actionPosY });
-  const { palette } = useTheme();
+  const isShown = useSelection({ componentId, posX, posY, width, height });
 
   return isShown ? (
     <>
-      {/* This is the panel on which options of the components are show. */}
-      <motion.div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          x: actualX,
-          y: actualActionPosY,
-          backgroundColor: palette.primary[400],
-          height: 24,
-        }}
-      >
-        <ActionBar componentId={componentId} />
-      </motion.div>
-
       <PlainOutline posX={actualX} posY={actualY} width={width} height={height} />
       {isResizable && (
         <ResizeableFrame
@@ -66,7 +46,7 @@ function ActualSelection({ componentId }) {
   ) : null;
 }
 
-function useSelection({ componentId, posX, posY, width, height, actionPosY }) {
+function useSelection({ componentId, posX, posY, width, height }) {
   const [isShown, setIsShown] = useState(false);
   const { getState, subscribe } = useComponentRegionStoreApi();
 
@@ -77,11 +57,6 @@ function useSelection({ componentId, posX, posY, width, height, actionPosY }) {
         posY.set(state.y);
         width.set(state.width);
         height.set(state.height);
-
-        // If the action bar overflows from the top, then show it to the bottom of
-        // the component.
-        const y = state.y - 23 >= 0 ? state.y - 23 : state.y + state.height - 1;
-        actionPosY.set(y);
         setIsShown(true);
         return;
       }
@@ -90,14 +65,13 @@ function useSelection({ componentId, posX, posY, width, height, actionPosY }) {
       posY.set(0);
       width.set(0);
       height.set(0);
-      actionPosY.set(0);
       setIsShown(false);
     };
 
     // Initialize with the first value.
     updateMotion(getState().regionMap[componentId]);
     return subscribe(updateMotion, (state) => state.regionMap[componentId]);
-  }, [actionPosY, componentId, getState, height, posX, posY, subscribe, width]);
+  }, [componentId, getState, height, posX, posY, subscribe, width]);
 
   return isShown;
 }
