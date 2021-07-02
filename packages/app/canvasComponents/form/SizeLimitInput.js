3f26a3ed3593f9691d42464504d242bf4df14518
Sharad Chand <pepsighan@sharadchand.com>
2021-07-02 09:28:16 +0545

    Migrate size limit input

--- packages/app/canvasComponents/form/SizeLimitInput.js
+++ packages/app/canvasComponents/form/SizeLimitInput.js
@@ -1,15 +1,4 @@
-import {
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Portal,
-} from '@chakra-ui/react';
-import { CheckIcon, ChevronDownIcon } from '@modulz/radix-icons';
+import { InputAdornment, MenuItem, Select, TextField, Typography } from '@material-ui/core';
 import { useCallback } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { parsePositiveInteger } from 'utils/parser';
@@ -22,71 +11,53 @@ export default function SizeLimitInput({ name, label }) {
   const unit = useWatch({ control, name: `${name}.unit` });
 
   return (
-    <InputGroup>
-      <InputLeftElement
-        pointerEvents="none"
-        fontSize="sm"
-        height="100%"
-        width={16}
-        color="gray.600"
-        justifyContent="flex-end"
-        pr={2}
-      >
-        {label}
-      </InputLeftElement>
-
-      <Input
-        value={size}
-        onChange={useCallback(
-          (event) => {
-            setValue(`${name}.size`, parsePositiveInteger(event.target.value) || 0);
-          },
-          [name, setValue]
-        )}
-        size="sm"
-        bg="white"
-        autoComplete="off"
-        flex={1}
-        placeholder="Auto"
-        sx={{ paddingInlineStart: 16 }}
-        pb="1px" // Align the input text with the label.
-      />
-
-      <InputRightElement height="100%" width={8}>
-        <Menu placement="bottom-end">
-          {({ isOpen }) => (
-            <>
-              <MenuButton>
-                <ChevronDownIcon />
-              </MenuButton>
-              <Portal>
-                <MenuList
-                  bg="white"
-                  zIndex="dropdown"
-                  minW="auto"
-                  width="60px"
-                  display={!isOpen ? 'none' : null}
-                >
-                  {units.map((it) => (
-                    <MenuItem
-                      key={it}
-                      onClick={() =>
-                        setValue(`${name}.unit`, it, {
-                          shouldDirty: true,
-                          shouldValidate: true,
-                        })
-                      }
-                      fontSize="sm"
-                    >
-                      {it} {unit === it && <CheckIcon />}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </Portal>
-            </>
-          )}
-        </Menu>
-      </InputRightElement>
-    </InputGroup>
+    <TextField
+      name={name}
+      value={size}
+      onChange={useCallback(
+        (event) => {
+          setValue(`${name}.size`, parsePositiveInteger(event.target.value) || 0);
+        },
+        [name, setValue]
+      )}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <Typography variant="body2">{label}</Typography>
+          </InputAdornment>
+        ),
+        endAdornment: (
+          <InputAdornment position="end">
+            <Select
+              value={unit}
+              onChange={useCallback(
+                (event) =>
+                  setValue(`${name}.unit`, event.target.value, {
+                    shouldDirty: true,
+                    shouldValidate: true,
+                  }),
+                [name, setValue]
+              )}
+              sx={{
+                '& .MuiOutlinedInput-notchedOutline': {
+                  border: 0,
+                },
+              }}
+            >
+              {units.map((it) => (
+                <MenuItem key={it} value={it}>
+                  {it}
+                </MenuItem>
+              ))}
+            </Select>
+          </InputAdornment>
+        ),
+      }}
+      sx={{
+        '& .MuiOutlinedInput-root': {
+          paddingRight: 0,
+        },
+      }}
+    />
   );
 }
