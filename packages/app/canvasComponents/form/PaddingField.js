ea3e5c03b6eac76a211a87cca702f089c3de5a1e
Sharad Chand <pepsighan@sharadchand.com>
2021-07-02 10:23:05 +0545

    Migrate the margin and padding fields

--- packages/app/canvasComponents/form/PaddingField.js
+++ packages/app/canvasComponents/form/PaddingField.js
@@ -1,24 +1,22 @@
-import { Grid, GridItem, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Box, Stack, Typography, TextField, InputAdornment } from '@material-ui/core';
 import { useCallback } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { parsePositiveInteger } from 'utils/parser';
 
 export default function PaddingField({ name }) {
   return (
-    <Grid templateColumns="repeat(3, minmax(0, 1fr))">
-      <GridItem colStart={2} colEnd={3}>
+    <Box>
+      <Typography variant="body2" sx={{ mb: 1 }}>
+        Padding
+      </Typography>
+
+      <Stack direction="row" spacing={1}>
         <NumberInputWithLabel name={`${name}.top`} label="T" />
-      </GridItem>
-      <GridItem colStart={1} colEnd={2}>
-        <NumberInputWithLabel name={`${name}.left`} label="L" />
-      </GridItem>
-      <GridItem colStart={3}>
         <NumberInputWithLabel name={`${name}.right`} label="R" />
-      </GridItem>
-      <GridItem colStart={2} colEnd={3}>
         <NumberInputWithLabel name={`${name}.bottom`} label="B" />
-      </GridItem>
-    </Grid>
+        <NumberInputWithLabel name={`${name}.left`} label="L" />
+      </Stack>
+    </Box>
   );
 }
 
@@ -27,34 +25,27 @@ function NumberInputWithLabel({ name, label }) {
   const value = useWatch({ control, name });
 
   return (
-    <InputGroup>
-      <InputLeftElement
-        pointerEvents="none"
-        fontSize="sm"
-        height="100%"
-        width={6}
-        justifyContent="flex-end"
-        pr={1}
-        color="gray.500"
-      >
-        {label}
-      </InputLeftElement>
-      <Input
-        value={value}
-        onChange={useCallback(
-          (event) => {
-            setValue(name, parsePositiveInteger(event.target.value) || 0);
-          },
-          [name, setValue]
-        )}
-        size="sm"
-        bg="white"
-        autoComplete="off"
-        pb="1px" // Align the input text with the label.
-        sx={{
-          paddingInlineStart: 6,
-        }}
-      />
-    </InputGroup>
+    <TextField
+      name={name}
+      value={value}
+      onChange={useCallback(
+        (event) => {
+          setValue(name, parsePositiveInteger(event.target.value) || 0);
+        },
+        [name, setValue]
+      )}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <Typography variant="body2">{label}</Typography>
+          </InputAdornment>
+        ),
+      }}
+      sx={{
+        '& .MuiOutlinedInput-root': {
+          paddingLeft: 1,
+        },
+      }}
+    />
   );
 }
