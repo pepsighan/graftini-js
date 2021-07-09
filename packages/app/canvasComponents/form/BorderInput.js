d149cfd43dbac99ac1586ee90295c3dbc58d7d63
Sharad Chand <pepsighan@sharadchand.com>
2021-07-09 19:16:22 +0545

    Show the default value if no border has been set

--- packages/app/canvasComponents/form/BorderInput.js
+++ packages/app/canvasComponents/form/BorderInput.js
@@ -1,11 +1,14 @@
-import { rgbaToCss } from '@graftini/bricks';
 import { InputAdornment, TextField, Typography } from '@material-ui/core';
 import { useCallback, useState } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
 import ColorBox from './ColorBox';
 
 export default function BorderInput({ name }) {
   const { control } = useFormContext();
+
+  // Since all the borders are going to be same for now. Lets just show the top.
+  const border = useWatch({ control, name: `${name}.top` });
+
   const [open, setOpen] = useState(null);
 
   const onOpen = useCallback((event) => {
@@ -21,7 +24,7 @@ export default function BorderInput({ name }) {
         <>
           <TextField
             onClick={onOpen}
-            value="Solid"
+            value={border?.style ?? '-'}
             InputProps={{
               readOnly: true,
               startAdornment: (
@@ -30,11 +33,11 @@ export default function BorderInput({ name }) {
                 </InputAdornment>
               ),
               // Show a chessboard bg to signify if there is transparency in the color.
-              endAdornment: (
+              endAdornment: border ? (
                 <InputAdornment position="end">
-                  <ColorBox value={{ r: 0, g: 0, b: 0 }} />
+                  <ColorBox value={border.color} />
                 </InputAdornment>
-              ),
+              ) : null,
             }}
             sx={{
               '& .MuiOutlinedInput-root, & .MuiOutlinedInput-input': {
