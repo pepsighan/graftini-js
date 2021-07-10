7965cff512063fce4fd0e2d5065a350da62e6f6c
Sharad Chand <pepsighan@sharadchand.com>
2021-07-10 17:29:51 +0545

    Allow wrapping components with right click

--- packages/app/components/editor/ComponentContextMenu.tsx
+++ packages/app/components/editor/ComponentContextMenu.tsx
@@ -1,5 +1,6 @@
-import { useOnDelete } from '@graftini/graft';
+import { newComponentNode, useEditorStore, useOnDelete } from '@graftini/graft';
 import { MenuItem } from '@material-ui/core';
+import Box from 'canvasComponents/Box';
 import useContextMenu from 'hooks/useContextMenu';
 import { createContext, PropsWithChildren, useCallback, useContext } from 'react';
 import { useDesignerState } from 'store/designer';
@@ -49,11 +50,47 @@ function useOnDeleteClick({ componentId, onClose }) {
 }
 
 function useOnWrapWithBox({ componentId, onClose }) {
+  const immerSet = useEditorStore(useCallback((state) => state.immerSet, []));
+
   return useCallback(() => {
     if (!componentId) {
       return;
     }
 
+    immerSet((state) => {
+      // Get the component which is to be wrapped.
+      const component = state.componentMap[componentId];
+
+      // Create a new box component which has no width and height of
+      // its own. Its going to stick tight to the component.
+      const wrapBoxComponent = newComponentNode({
+        type: 'Box',
+        isCanvas: true,
+        childAppendDirection: 'vertical',
+        defaultProps: {
+          ...Box.graftOptions.defaultProps,
+          width: 'auto',
+          height: 'auto',
+        },
+      });
+
+      // Register the new box component.
+      state.componentMap[wrapBoxComponent.id] = wrapBoxComponent;
+
+      // Replace the wrapped component with the wrapper box in the parent.
+      const parent = state.componentMap[component.parentId!];
+      const componentIndex = parent.childrenNodes.indexOf(component.id);
+      parent.childrenNodes[componentIndex] = wrapBoxComponent.id;
+      wrapBoxComponent.parentId = parent.id;
+
+      // Add the wrapped component to the wrapper box.
+      wrapBoxComponent.childrenNodes = [component.id];
+      // Replace the parent of the wrapped component with the wrapper.
+      component.parentId = wrapBoxComponent.id;
+
+      // Its done.
+    });
+
     onClose();
-  }, [componentId, onClose]);
+  }, [componentId, immerSet, onClose]);
 }
