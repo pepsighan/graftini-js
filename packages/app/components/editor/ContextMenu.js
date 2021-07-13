2a3af9b5e6641187db37ad7a444faf4b286a0db9
Sharad Chand <pepsighan@sharadchand.com>
2021-07-13 11:12:37 +0545

    Use custom context menu that has no overlay

--- packages/app/components/editor/ContextMenu.js
+++ packages/app/components/editor/ContextMenu.js
@@ -1,39 +1,24 @@
-import { Menu } from '@material-ui/core';
-import { useCallback } from 'react';
+import { ClickAwayListener, Paper } from '@material-ui/core';
 import { leftSideBarWidth, navBarHeight } from 'utils/constants';
 
 export default function ContextMenu({ context, onClose, isCorrectionNeeded = false, children }) {
-  // Also close it if right click is pressed again.
-  const onContextMenuAgain = useCallback(
-    (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      onClose();
-    },
-    [onClose]
-  );
-
-  return (
-    <Menu
-      open={!!context}
-      onClose={onClose}
-      onContextMenu={onContextMenuAgain}
-      anchorReference="anchorPosition"
-      anchorPosition={
-        context
-          ? {
-              // The correction may be needed if the menu is to be shown on right clicking the
-              // components within the canvas. In that case, the position of the context menu is
-              // relative to the iframe which needs to be corrected for it to be shown in the
-              // context of the main document.
-              left: context.x + (isCorrectionNeeded ? leftSideBarWidth : 0),
-              top: context.y + (isCorrectionNeeded ? navBarHeight : 0),
-            }
-          : null
-      }
-      MenuListProps={{ sx: { width: 150 } }}
-    >
-      {children}
-    </Menu>
-  );
+  return context ? (
+    <ClickAwayListener onClickAway={onClose}>
+      <Paper
+        sx={{
+          position: 'fixed',
+          zIndex: '100',
+          // The correction may be needed if the menu is to be shown on right clicking the
+          // components within the canvas. In that case, the position of the context menu is
+          // relative to the iframe which needs to be corrected for it to be shown in the
+          // context of the main document.
+          left: context?.x + (isCorrectionNeeded ? leftSideBarWidth : 0),
+          top: context?.y + (isCorrectionNeeded ? navBarHeight : 0),
+          width: 150,
+        }}
+      >
+        {children}
+      </Paper>
+    </ClickAwayListener>
+  ) : null;
 }
--- packages/app/hooks/useContextMenu.ts
+++ packages/app/hooks/useContextMenu.ts
@@ -8,18 +8,11 @@ export default function useContextMenu({ data }: { data?: any } = {}) {
       event.preventDefault();
       event.stopPropagation();
 
-      // Close the context menu is open is clicked twice.
-      // This probably means that the user has right clicked again on
-      // a separate place on the same surface.
-      setContext((context: any) =>
-        !context
-          ? {
-              x: event.clientX,
-              y: event.clientY,
-              ...(data ?? {}),
-            }
-          : null
-      );
+      setContext({
+        x: event.clientX,
+        y: event.clientY,
+        ...(data ?? {}),
+      });
     },
     [data]
   );
