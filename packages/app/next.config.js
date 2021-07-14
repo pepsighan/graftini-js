ce1dca577a57a247fd1b1dca69240ae774ebd38f
Sharad Chand <pepsighan@sharadchand.com>
2021-07-14 20:14:15 +0545

    Add config for enabling sentry

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -2,9 +2,12 @@ const withCustomTranspilation = require('next-transpile-modules')(['lodash-es'])
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const { withSentryConfig } = require('@sentry/nextjs');
 
 module.exports = withBundleAnalyzer(
-  withCustomTranspilation({
-    productionBrowserSourceMaps: true,
-  })
+  withSentryConfig(
+    withCustomTranspilation({
+      productionBrowserSourceMaps: true,
+    })
+  )
 );
