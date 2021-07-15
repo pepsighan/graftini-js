71ee471ed4bc27df76a7ccc36f24338858604d3b
Sharad Chand <pepsighan@sharadchand.com>
2021-07-15 12:29:30 +0545

    Only generate config in production

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -3,11 +3,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const { withSentryConfig } = require('@sentry/nextjs');
+const withPlugins = require('next-compose-plugins');
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-module.exports = withBundleAnalyzer(
-  withSentryConfig(
-    withCustomTranspilation({
-      productionBrowserSourceMaps: true,
-    })
-  )
-);
+module.exports = withPlugins([
+  withBundleAnalyzer,
+  [withSentryConfig, {}, [PHASE_PRODUCTION_BUILD]],
+  [withCustomTranspilation, { productionBrowserSourceMaps: true }],
+]);
