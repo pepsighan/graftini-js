cec7aa5ca1958860da58933b3855ee4c6e2cc3e7
Sharad Chand <pepsighan@sharadchand.com>
2021-07-16 13:51:01 +0545

    Add a popver to select a bg

--- packages/app/canvasComponents/form/BackgroundImageInput.js
+++ packages/app/canvasComponents/form/BackgroundImageInput.js
@@ -1,38 +1,64 @@
-import { Box, InputAdornment, TextField, Typography } from '@material-ui/core';
+import { Box, InputAdornment, Popover, TextField, Typography } from '@material-ui/core';
+import { useCallback, useState } from 'react';
 import { wideLabelAlignmentStyle } from './formLabels';
 
 export default function BackgroundImageInput() {
   const imageUrl = null;
 
+  const [open, setOpen] = useState(null);
+  const onOpen = useCallback((event) => setOpen(event.currentTarget), []);
+  const onClose = useCallback(() => setOpen(null), []);
+
   return (
-    <TextField
-      placeholder="Add"
-      InputProps={{
-        readOnly: true,
-        startAdornment: (
-          <InputAdornment position="start" sx={wideLabelAlignmentStyle}>
-            <Typography variant="body2">Image</Typography>
-          </InputAdornment>
-        ),
-        endAdornment: imageUrl ? (
-          <InputAdornment position="end">
-            <Box
-              sx={{
-                width: 16,
-                height: 16,
-                bgcolor: 'grey.200',
-                borderRadius: 1,
-                backgroundImage: `url(${imageUrl})`,
-              }}
-            />
-          </InputAdornment>
-        ) : null,
+    <>
+      <TextField
+        placeholder="Add"
+        onClick={onOpen}
+        InputProps={{
+          readOnly: true,
+          startAdornment: (
+            <InputAdornment position="start" sx={wideLabelAlignmentStyle}>
+              <Typography variant="body2">Image</Typography>
+            </InputAdornment>
+          ),
+          endAdornment: imageUrl ? (
+            <InputAdornment position="end">
+              <Box
+                sx={{
+                  width: 16,
+                  height: 16,
+                  bgcolor: 'grey.200',
+                  borderRadius: 1,
+                  backgroundImage: `url(${imageUrl})`,
+                }}
+              />
+            </InputAdornment>
+          ) : null,
+        }}
+        sx={{
+          '& .MuiOutlinedInput-root, & .MuiOutlinedInput-input': {
+            cursor: 'pointer',
+          },
+        }}
+      />
+
+      <ImagePickerPopover open={open} onClose={onClose} />
+    </>
+  );
+}
+
+function ImagePickerPopover({ value, onChange, open, onClose }) {
+  return (
+    <Popover
+      open={!!open}
+      anchorEl={open}
+      onClose={onClose}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
       }}
-      sx={{
-        '& .MuiOutlinedInput-root, & .MuiOutlinedInput-input': {
-          cursor: 'pointer',
-        },
-      }}
-    />
+    >
+      <Box sx={{ p: 1 }}>Hello World</Box>
+    </Popover>
   );
 }
