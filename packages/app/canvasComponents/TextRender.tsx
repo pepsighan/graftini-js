807c17fae46177b9421e6182616b604ffaf3ad76
Sharad Chand <pepsighan@sharadchand.com>
2021-07-17 10:31:25 +0545

    Do not mutate object

--- packages/app/canvasComponents/TextRender.tsx
+++ packages/app/canvasComponents/TextRender.tsx
@@ -2,7 +2,7 @@ import { Text } from '@graftini/bricks';
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import { TextComponentProps } from './Text';
-import { Link } from './textEditor/entities';
+import { EntityKind, Link } from './textEditor/entities';
 
 export default function TextRender({ content }: TextComponentProps) {
   const { route, query } = useRouter();
@@ -13,28 +13,35 @@ export default function TextRender({ content }: TextComponentProps) {
       blocks: content.blocks,
     };
 
-    modified.entityMap = Object.keys(content.entityMap).map((key) => {
-      const entity = content.entityMap[key];
-      const linkData = entity.data.link as Link;
+    modified.entityMap = Object.keys(content.entityMap)
+      .map((key) => {
+        const entity = { ...content.entityMap[key] };
+        if (entity.type !== EntityKind.Link) {
+          // Only link kinds are supported now.
+          return null;
+        }
 
-      let to: string | undefined;
+        const linkData = entity.data.link as Link;
 
-      if (linkData?.pageId) {
-        // We cannot have the routing system for the preview with actual path, so we will just
-        // use the page ids. The actual deployment is able to do it because it knows all about what
-        // routes exist based on the files generated.
-        to = `${route.replace('[projectId]', query.projectId as string)}?page=${linkData.pageId}`;
-      }
+        let to: string | undefined;
 
-      entity.data = {
-        link: {
-          to: linkData?.pageId ? to : undefined,
-          href: !linkData?.pageId ? linkData?.href : undefined,
-        },
-      };
+        if (linkData.pageId) {
+          // We cannot have the routing system for the preview with actual path, so we will just
+          // use the page ids. The actual deployment is able to do it because it knows all about what
+          // routes exist based on the files generated.
+          to = `${route.replace('[projectId]', query.projectId as string)}?page=${linkData.pageId}`;
+        }
 
-      return entity;
-    });
+        entity.data = {
+          link: {
+            to: linkData.pageId ? to : undefined,
+            href: !linkData.pageId ? linkData.href : undefined,
+          },
+        };
+
+        return entity;
+      })
+      .filter((it) => !!it);
 
     return modified;
   }, [content.blocks, content.entityMap, query.projectId, route]);
