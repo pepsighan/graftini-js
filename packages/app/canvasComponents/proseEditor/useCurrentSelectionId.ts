5eaec9148696a575ac0ac259cd41d142a648d9ac
Sharad Chand <pepsighan@sharadchand.com>
2021-07-19 15:45:29 +0545

    Use the select id based on whether editing or not

--- packages/app/canvasComponents/proseEditor/useCurrentSelectionId.ts
+++ packages/app/canvasComponents/proseEditor/useCurrentSelectionId.ts
@@ -1,10 +1,25 @@
+import { useComponentId } from '@graftini/graft';
 import { useProseEditor } from './ProseEditorContext';
+import useGetSelection from './useGetSelection';
+import useIsEditing from './useIsEditing';
 
 /**
  * A unique id for the current selection.
  */
 export default function useCurrentSelectionId() {
   const { getCurrentSelection } = useProseEditor();
+  const isEditing = useIsEditing();
+  const componentId = useComponentId();
+
+  // This selection is based on whether the component is actively editing or not.
+  // If not in editing, then the whole thing is deemed to be selected.
+  const getSelection = useGetSelection(componentId);
+
   const selection = getCurrentSelection();
-  return selection ? `${selection.from}-${selection.to}` : 'no-selection';
+
+  // We need to the id for a selection based on whether they are editing or not.
+  // Otherwise, the text options form looses focus.
+  const resolvedSelection = isEditing ? selection : getSelection();
+
+  return selection ? `${resolvedSelection.from}-${resolvedSelection.to}` : 'no-selection';
 }
