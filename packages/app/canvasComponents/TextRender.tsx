70d55e0dd309e1b33d39d17fa6ebcf15efd19dfd
Sharad Chand <pepsighan@sharadchand.com>
2021-07-19 17:47:29 +0545

    Use the default style that forms the base for the text component

--- packages/app/canvasComponents/TextRender.tsx
+++ packages/app/canvasComponents/TextRender.tsx
@@ -1,6 +1,9 @@
 import { Text } from '@graftini/bricks';
+import { defaultTextFormValues } from './proseEditor/formFields';
 import { TextComponentProps } from './Text';
 
 export default function TextRender({ content }: TextComponentProps) {
-  return <Text content={content} />;
+  // For anything that is not styled, they have the default look. The styles
+  // that are configured by the user will be overridden by the content itself.
+  return <Text {...defaultTextFormValues} content={content} />;
 }
