0293db1c7ccf92ccc087f8046bc7a09dc5353c42
Sharad Chand <pepsighan@sharadchand.com>
2021-07-24 18:14:51 +0545

    Use the same react-use lib

--- packages/app/components/editor/SavingStatus.js
+++ packages/app/components/editor/SavingStatus.js
@@ -2,19 +2,15 @@ import { IconButton, Tooltip } from '@material-ui/core';
 import { ArchiveIcon } from '@modulz/radix-icons';
 import { useCallback } from 'react';
 import { useDesignerState } from 'store/designer';
-import { useBeforeunload } from 'react-beforeunload';
+import { useBeforeUnload } from 'react-use';
 
 export default function SavingStatus() {
   const isSaving = useDesignerState(useCallback((state) => state.isSaving, []));
 
   // Stop the tab from being closed when the project is actively being saved.
-  useBeforeunload(() => {
-    if (isSaving) {
-      // This text is not shown on the latest browsers. The browser shows a default
-      // prompt which works for us.
-      return 'The changes are being saved. Are you sure you want to exit? ';
-    }
-  });
+  // This text is not shown on the latest browsers. The browser shows a default
+  // prompt which works for us.
+  useBeforeUnload(isSaving, 'The changes are being saved. Are you sure you want to exit?');
 
   return (
     <Tooltip title={isSaving ? 'Saving your project right now.' : 'Your project is saved.'}>
