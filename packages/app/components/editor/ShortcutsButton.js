644b9a1b6cb925fa9df343692f8c56adec11f2d1
Sharad Chand <pepsighan@sharadchand.com>
2021-07-27 11:04:18 +0545

    Only show the badge for the first time

--- packages/app/components/editor/ShortcutsButton.js
+++ packages/app/components/editor/ShortcutsButton.js
@@ -2,20 +2,28 @@ import { Badge, Box, IconButton, Popover, Stack, Tooltip, Typography } from '@ma
 import { MixIcon } from '@modulz/radix-icons';
 import { useCallback, useState } from 'react';
 import { isMacOs } from 'react-device-detect';
+import { useLocalStorage } from 'react-use';
 
 const CtrlKey = isMacOs ? '⌘' : 'Ctrl';
 
 export default function ShortcutsButton() {
   const [open, setOpen] = useState(null);
+  const [value, setValue] = useLocalStorage('graftini-shortcut-viewed', 'false');
 
-  const onOpen = useCallback((event) => setOpen(event.currentTarget), []);
+  const onOpen = useCallback(
+    (event) => {
+      setOpen(event.currentTarget);
+      setValue('true');
+    },
+    [setValue]
+  );
   const onClose = useCallback(() => setOpen(null), []);
 
   return (
     <>
       <Tooltip title="Shortcuts">
         <IconButton onClick={onOpen}>
-          <Badge variant="dot" color="info">
+          <Badge variant="dot" color="info" invisible={value === 'true'}>
             <MixIcon />
           </Badge>
         </IconButton>
