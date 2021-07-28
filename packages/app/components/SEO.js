e98d3a2d99a77f089e6bf0a3afc3aae0c8ca6ab0
Sharad Chand <pepsighan@sharadchand.com>
2021-07-28 17:19:30 +0545

    Use open graph seo tags

--- packages/app/components/SEO.js
+++ packages/app/components/SEO.js
@@ -1,16 +1,40 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import config, { Environment } from 'utils/config';
 
-// TODO: Add open graph meta properties and twitter properties.
+function domainName() {
+  switch (config.ENV) {
+    case Environment.Development:
+      return 'https://development.graftini.xyz';
+    case Environment.Production:
+      return 'https://www.graftini.com';
+    case Environment.Local:
+    default:
+      return 'http://localhost:3000';
+  }
+}
+
 export default function SEO({ title, description }) {
+  const expandedTitle = title ? `${title} - Graftini` : 'Graftini';
+
+  const { pathname } = useRouter();
+  const domain = domainName();
+  const url = domain + pathname;
+
   return (
     <Head>
-      <title>{title ? `${title} - Graftini` : 'Graftini'}</title>
+      <title>{expandedTitle}</title>
       <meta
         name="description"
         content={
           description ?? 'Deploy your web apps without writing any code in less than 10 minutes.'
         }
       />
+
+      <meta property="og:title" content={expandedTitle} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
     </Head>
   );
 }
