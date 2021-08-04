515340051bead889393cf9307b77d5507a9ae579
Sharad Chand <pepsighan@sharadchand.com>
2021-08-04 15:27:14 +0545

    Add a hook to get the used colors in the page

---
+++ packages/app/hooks/useGetPalette.ts
@@ -0,0 +1,45 @@
+import { RGBA, rgbaToCss } from '@graftini/bricks';
+import { useEditorStore } from '@graftini/graft';
+import { isEqual } from 'lodash-es';
+import { useCallback, useMemo } from 'react';
+
+/**
+ * Gets the palette in the given page.
+ */
+export default function useGetPalette(): RGBA[] {
+  const colors = useEditorStore(
+    useCallback((state) => {
+      const colors: { [key: string]: RGBA } = {};
+
+      Object.keys(state.componentMap).forEach((key) => {
+        const component = state.componentMap[key];
+        if (component.type === 'Text') {
+          // Skip the text component for now.
+          // TODO: We will come back to it later.
+          return;
+        }
+
+        if (component.type === 'Root') {
+          // Also skip the root component for now.
+          // TODO: We will come back to it later.
+          return;
+        }
+
+        const color = component.props.color;
+        colors[rgbaToCss(color)] = color;
+
+        const border = component.props.border?.top;
+        if (border) {
+          colors[rgbaToCss(border.color)] = border.color;
+        }
+
+        // TODO: Add box shadow colors to the fold when it is added.
+      });
+
+      return colors;
+    }, []),
+    useCallback((left, right) => isEqual(left, right), [])
+  );
+
+  return useMemo(() => Object.values(colors), [colors]);
+}
