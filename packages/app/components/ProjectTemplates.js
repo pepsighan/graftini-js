21c0d31487c077bf37bb37256c32c12e4c73c524
Sharad Chand <pepsighan@sharadchand.com>
2021-08-05 14:25:34 +0545

    Add a button for blank project

--- packages/app/components/ProjectTemplates.js
+++ packages/app/components/ProjectTemplates.js
@@ -1,22 +1,68 @@
-import { Box, Grid, Paper, Typography } from '@material-ui/core';
+import { Box, Button, Grid, Paper, Typography } from '@material-ui/core';
+import { FileIcon } from '@modulz/radix-icons';
 
 export default function ProjectTemplates() {
   return (
     <Box>
-      <Typography variant="subtitle2" color="textSecondary" sx={{ mb: 1 }}>
+      <Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
         Kick start your project with a template that suits your needs. If you want a clean slate,
         start from scratch.
       </Typography>
 
       <Grid container spacing={2}>
-        {Array(3)
+        <Grid item xs={4}>
+          <BlankProject />
+        </Grid>
+        {Array(4)
           .fill(0)
           .map((_, index) => (
             <Grid key={index} item xs={4}>
-              <Paper sx={{ bg: 'grey.200', height: 200 }} />
+              <Project />
             </Grid>
           ))}
       </Grid>
     </Box>
+  );
+}
+
+function BlankProject() {
+  return (
+    <Button sx={{ flexDirection: 'column', width: '100%', p: 0 }}>
+      <Paper
+        sx={{
+          height: 200,
+          bgcolor: 'grey.100',
+          color: 'grey.400',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <FileIcon width={64} height={64} />
+      </Paper>
+      <Typography variant="subtitle2" sx={{ px: 1, py: 0.5 }}>
+        Blank
+      </Typography>
+    </Button>
+  );
+}
+
+function Project() {
+  return (
+    <Button sx={{ flexDirection: 'column', width: '100%', p: 0 }}>
+      <Paper
+        sx={{
+          height: 200,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      />
+      <Typography variant="subtitle2" sx={{ px: 1, py: 0.5 }}>
+        Portfolio
+      </Typography>
+    </Button>
   );
 }
