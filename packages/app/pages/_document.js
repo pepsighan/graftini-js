f7150ac3c456bcf53e5b539adc376d1e232741e8
Sharad Chand <pepsighan@sharadchand.com>
2021-08-06 16:14:40 +0545

    Add google analytics

---
+++ packages/app/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5SF7LFKFM7" />
+          <script>{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5SF7LFKFM7');
+          `}</script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
